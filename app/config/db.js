0f2e19b8b4f47247ff10630853a343f707f9215e
CarlosGarcia20 <cargarcia085@gmail.com>
2025-05-28 23:02:35 -0700

    Refactorización de la configuración de la base de datos: se eliminó el pool de conexiones para Neon y se reestructuró el uso de variables de entorno para la conexión a la base de datos.

--- app/config/db.js
+++ app/config/db.js
@@ -2,18 +2,30 @@ import pkg from "pg";
 const { Pool } = pkg;
 import 'dotenv/config';
 
-// Crear pool de conexiones
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // Necesario para Neon
-    }
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT
 });
 
-// Probar la conexión
-pool.connect()
-    .then(() => console.log("📡 Conectado a Neon PostgreSQL"))
-    .catch(err => console.error("❌ Error de conexión a Neon:", err));
-
 export default pool;
+
+// Esta parte es para poderse conectar a la BD de Neon Database
+
+// Crear pool de conexiones
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false // Necesario para Neon
+//     }
+// });
+
+// // Probar la conexión
+// pool.connect()
+//     .then(() => console.log("📡 Conectado a Neon PostgreSQL"))
+//     .catch(err => console.error("❌ Error de conexión a Neon:", err));
+
+// export default pool;
 
