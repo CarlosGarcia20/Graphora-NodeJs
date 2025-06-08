a19910833915c88e147054ed71542dc393993f8a
CarlosGarcia20 <cargarcia085@gmail.com>
2025-06-08 16:07:36 -0700

    Modificacion para que el frontend pueda trabajar comodo con los tokens (temporal)

--- app/config/config.js
+++ app/config/config.js
@@ -4,7 +4,7 @@ export const PORT = process.env.PORT || 4000;
 const config = {
     jwtSecret: process.env.JWT_SECRET || 'secreto_super_seguro',
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || 'refresh_secreto',
-    jwtExpiresIn: '15m',
+    jwtExpiresIn: '24h',
     jwtRefreshExpiresIn: '7d'
 };
 
