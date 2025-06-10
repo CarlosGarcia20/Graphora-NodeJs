7d71d40d15792070db276514b256eddf4676a118
CarlosGarcia20 <cargarcia085@gmail.com>
2025-06-10 09:45:50 -0700

    Añadido la URL del frontend a los cors

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -1,8 +1,10 @@
 import cors from 'cors'
+import 'dotenv/config';
 
 const ACCEPTED_ORIGINS = [
     'http://localhost:4200',
-    'http://localhost:4200/'
+    'http://localhost:4200/',
+    process.env.FRONTEND_PROD_URL
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
