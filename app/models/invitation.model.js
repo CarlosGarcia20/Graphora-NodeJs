eb0db61617e279b5391d355c081669915041eb77
CarlosGarcia20 <cargarcia085@gmail.com>
2025-06-10 20:07:43 -0700

    Refactorizacion al socker send-invitation para mayor claridad y estructura; añadido el user model  para usar los datos del usuario

--- app/models/invitation.model.js
+++ app/models/invitation.model.js
@@ -3,23 +3,16 @@ import pool from "../config/db.js";
 export class InvitationModel {
     static async sendInvitation({ userId, diagramId, InvitedUserEmail }) {
         try {
-            const { rowCount } = await pool.query(
+            const { rows } = await pool.query(
                 `INSERT INTO diagram_invitations
                 (diagram_id, invited_user_email, invited_by_user_id)
-                VALUES ($1, $2, $3)`,
+                VALUES ($1, $2, $3)
+                RETURNING *`,
                 [
                     diagramId,
                     InvitedUserEmail,
                     userId
                 ]
-            )
-
-            console.log(rowCount)
-
-            const { rows } = await pool.query(
-                `SELECT name, lastname FROM users
-                WHERE userid = $1`,
-                [userId]
             )
             
             return { success: true, data: rows[0] }
--- app/sockets/connection_handler.js
+++ app/sockets/connection_handler.js
@@ -1,6 +1,7 @@
 import { compareSync } from "bcrypt";
 import { DiagramModel } from "../models/diagram.model.js";
 import { InvitationModel } from "../models/invitation.model.js";
+import { UserModel } from "../models/users.models.js";
 
 const emailSockets = new Map();
 
@@ -17,15 +18,15 @@ export function setupSocketConnections(io) {
 
         socket.on('send-invitation', async ({ userId, diagramId, InvitedUserEmail }) => {
             try {
-                const userData = await InvitationModel.sendInvitation({ userId, diagramId, InvitedUserEmail })
+                const invitationData = await InvitationModel.sendInvitation({ userId, diagramId, InvitedUserEmail })
+
+                const userData = await UserModel.getUserById({ userId })
                 const diagramData = await DiagramModel.getDiagramInfo({ userId, diagramId })
-
-                console.log("Diagrama: " + diagramId)
-
                 const socketId = emailSockets.get(InvitedUserEmail)
                 if (socketId) {
                     io.to(socketId).emit('invitation', {
-                        diagramId,
+                        id: invitationData.data.id,
+                        diagramId: diagramData.data.id
                         from: `${userData.data.name} ${userData.data.lastname}`,
                         email: InvitedUserEmail,
                         message: `Has sido invitado a colaborar en el diagrama ${diagramData.data.name}`
