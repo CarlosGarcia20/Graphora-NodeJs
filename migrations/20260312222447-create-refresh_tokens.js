b61e4b610bd5eb1679a9e7de888c8d7ca2b73e76
CarlosGarcia20 <cargarcia085@gmail.com>
2026-03-12 15:30:53 -0700

    Se ha creado la migracion para crear la tabla de los refresh tokens

---
+++ migrations/20260312222447-create-refresh_tokens.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export async function up(queryInterface, Sequelize) {
+  /**
+   * Add altering commands here.
+   *
+   * Example:
+   * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+   */
+  await queryInterface.createTable('refresh_tokens', {
+    token: {
+      type: Sequelize.DataTypes.TEXT,
+      primaryKey: true,
+      allowNull: false,
+    },
+    userid: {
+      type: Sequelize.DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'userid'
+      },
+      onDelete: 'RESTRICT',
+      onUpdate: 'CASCADE'
+    },
+    expiresat: {
+      type: Sequelize.DataTypes.DATE,
+      allowNull: false
+    }
+  })
+}
+export async function down(queryInterface, Sequelize) {
+  /**
+   * Add reverting commands here.
+   *
+   * Example:
+   * await queryInterface.dropTable('users');
+   */
+}
