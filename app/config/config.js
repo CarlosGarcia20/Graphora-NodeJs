d09f5fb75d3b39ecfbc5953d745140483d304d5e
CarlosGarcia20 <cargarcia085@gmail.com>
2026-03-12 13:43:50 -0700

    Restructuracion del archivo de configuracion para tomar datos el archivo .env y que Sequelize se pueda conectar a la bd

--- app/config/config.js
+++ app/config/config.js
@@ -2,10 +2,18 @@ import 'dotenv/config';
 export const PORT = process.env.PORT || 4000;
 
 const config = {
-    jwtSecret: process.env.JWT_SECRET || 'secreto_super_seguro',
-    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET || 'refresh_secreto',
-    jwtExpiresIn: '24h',
-    jwtRefreshExpiresIn: '7d'
+    jwtSecret: process.env.JWT_SECRET,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+    jwtExpiresIn: process.env.JWTEXPIRESIN,
+    jwtRefreshExpiresIn: process.env.JWTREFRESHEXPIRESIN,
+    development: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect: 'postgres'
+    }
 };
 
 export default config;
