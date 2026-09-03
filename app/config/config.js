d8e3f68bef9a3ac14c72ff59f170983f66c81b93
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 12:04:26 -0700

    Cambios en los nombres de las variables de entorno para que tenga una nomenglatura consistente

--- app/config/config.js
+++ app/config/config.js
@@ -5,11 +5,8 @@ import { v2 as cloudinary } from 'cloudinary';
 const config = {
     jwtSecret: process.env.JWT_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-    jwtExpiresIn: process.env.JWTEXPIRESIN,
-    jwtRefreshExpiresIn: process.env.JWTREFRESHEXPIRESIN,
-    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-    cloudApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudApiSecret: process.env.CLOUDINARY_API_SECRET,
+    jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+    jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
     development: {
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
