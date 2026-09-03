4741d9996233f4db1a170e1598a5ee2178f31b7b
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 12:44:43 -0700

    Cambio de nombre en la variable para que tenga una correcta relacion con todas sus variables

--- app/schemas/register.js
+++ app/schemas/register.js
@@ -4,7 +4,7 @@ const registerSchema = zod.object({
     email: zod.string().email().toLowerCase(),
     password: zod.string().min(8),
     name: zod.string().min(3),
-    lastName: zod.string().min(3)
+    lastname: zod.string().min(3)
 });
 
 export const validateRegister = (input) => {
