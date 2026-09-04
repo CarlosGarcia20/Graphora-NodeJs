4d493951ab73d6b0c2f404a76e27757f586abd4b
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 18:23:32 -0700

    Se ha modificado el archivo de configuracion para que solo este archivo pueda manejar las variables de entorno y diferencie si se esta en produccion o en desarrollo

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 export const PORT = process.env.PORT || 4000;
+export const NODE_ENV = process.env.NODE_ENV || 'development'
+export const isProduction = NODE_ENV === 'production'
+export const isDevelopment = NODE_ENV === 'development'
 import { v2 as cloudinary } from 'cloudinary';
 
 const config = {
@@ -7,14 +10,25 @@ const config = {
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
     jwtExpiresIn: process.env.JWT_EXPIRES_IN,
     jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+
     development: {
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT
+    },
+
+    production: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
         port: process.env.DATABASE_PORT,
-        dialect: 'postgres'
+        ssl: { rejectUnauthorized: false }
     }
 };
 
-export default config;
+export const dbConfig = config[NODE_ENV] || config.development
+
+export default config
