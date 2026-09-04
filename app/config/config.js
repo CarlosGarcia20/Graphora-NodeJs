c5468df1645aa3e09d060f2177be1463dc604ff0
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 18:41:07 -0700

    Se agregó un valor que se elimino por accidente

--- app/config/config.js
+++ app/config/config.js
@@ -16,7 +16,8 @@ const config = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT
+        port: process.env.DATABASE_PORT,
+        dialect: 'postgres'
     },
 
     production: {
