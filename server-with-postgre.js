493ce91e9d0b94c763a1a9aa6cede16e6add1ce4
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 19:54:28 -0700

    Se inyectó el servicio de tokens en la aplicacion. Se corrigió el nombre del modelo de tokens para que utilice Pascal Case

--- server-with-postgre.js
+++ server-with-postgre.js
@@ -3,13 +3,20 @@ import { createApp } from "./app.js";
 import { LoginModel } from "./app/models/postgresql/auth.models.js";
 import { DiagramModel } from "./app/models/postgresql/diagram.model.js";
 import { UserModel } from "./app/models/postgresql/users.models.js";
-import { tokenModel } from "./app/models/postgresql/token.model.js";
+import { TokenModel } from "./app/models/postgresql/token.model.js";
+
+import { TokenService } from "./app/services/tokenService.js";
+
+const tokenService = new TokenService()
 
 const models = {
     loginModel: new LoginModel(),
     userModel: new UserModel(),
     diagramModel: new DiagramModel(),
-    tokenModel: new tokenModel()
+    tokenModel: new TokenModel()
 }
-
-createApp({ models })
+    
+const app = createApp({
+    models,
+    services: { tokenService }
+})
