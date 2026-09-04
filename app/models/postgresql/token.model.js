8cf377632f3516766e2b32610c1fc6b273d26188
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 20:06:49 -0700

    Se agregó un try/catch al guardar el token del usuario. Se modifico la manera en como se encuentra un token. Se agregó una funcion nueva para eliminar todos los tokens de un usuario

--- app/models/postgresql/token.model.js
+++ app/models/postgresql/token.model.js
@@ -1,31 +1,32 @@
 import pool from "../../config/db.js";
+import { mapPostgresError } from "./errors/postgresErrorMapper.js";
 
 export class TokenModel {
     saveUserToken = async({ userId, token, expiresAt }) => {
-        await pool.query(
-            `
-            INSERT INTO refresh_tokens (token, userid, expires_at)
-            VALUES ($1, $2, $3)
-            `,
-            [ token, userId, expiresAt ]
-        );
+        try {
+            await pool.query(
+                `
+                INSERT INTO refresh_tokens (token, userid, expires_at)
+                VALUES ($1, $2, $3)
+                `,
+                [ token, userId, expiresAt ]
+            );
+        } catch (error) {
+            throw mapPostgresError(error)
+        }
     }
 
     findToken = async({ token }) => {
         const { rows } = await pool.query(
-            `SELECT token FROM refresh_tokens
-            WHERE token = $1`,
+            `SELECT token, userid, expires_at
+            FROM refresh_tokens
+            WHERE token = $1 AND expires_at > NOW() `,
             [token]
         );
 
-        if (rows.length === 0) {
-            return { success: false, message: "No existe el token solicitado" }
-        }
+        if (rows.length === 0) return { success: false }
 
-        return {
-            success: true,
-            token: rows[0].token
-        }
+        return { success: true, data: rows[0] }
     }
     
     revokeToken = async({ token }) => {
@@ -33,5 +34,12 @@ export class TokenModel {
             `DELETE FROM refresh_tokens WHERE token = $1`,
             [token]
         );
+    }
+
+    revokeAllUserTokens = async({ userId }) => {
+        await pool.query(
+            `DELETE FROM refresh_tokens WHERE userid = $1`,
+            [userId]
+        )
     }
 }
