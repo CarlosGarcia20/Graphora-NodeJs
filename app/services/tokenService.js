d19c80fbaff686d3283f016834da92d76b19206a
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 19:52:58 -0700

    Se corrigió el archivo de token para mandarlo a services. Se agregó la funcion de verifyToken para poder verificar los tokens

---
+++ app/services/tokenService.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import config from '../config/config.js';
+
+/* 
+ * TODO
+ * Por el momento los usuarios no tienen roles pero en un futuro
+ * se agregaran roles para poder diferenciar a los usuarios de los
+ * administradores, etc.
+*/
+
+export class TokenService {
+   generateToken(userData) {
+      return jwt.sign(
+         {
+            userId: userData.userId
+         },
+         config.jwtSecret, { expiresIn: config.jwtExpiresIn }
+      )
+   }
+
+   generateRefreshToken(userData) {
+      return jwt.sign(
+         {
+            userId: userData.userId
+         },
+         config.jwtRefreshSecret, { expiresIn: config.jwtRefreshExpiresIn }
+      )
+   }
+
+   verifyToken(token) {
+      return jwt.verify(token, config.jwtSecret)
+   }
+
+   static verifyRefreshToken(token) {
+        return jwt.verify(token, config.jwtRefreshSecret);
+    } 
+}
