4ab2985c49b9832fe63551813cec552904004c4e
CarlosGarcia20 <cargarcia085@gmail.com>
2026-09-03 23:19:22 -0700

    Correcion a los manejos de errores con el token

--- app/middlewares/tokenMiddleware.js
+++ app/middlewares/tokenMiddleware.js
@@ -1,6 +1,6 @@
 export const requireAuth = ({ tokenService }) => {
     return (req, res, next) => {
-        const token = req.cookies.accessToken;
+        const token = req.cookies?.accessToken;
 
         if (!token) {
             return res.status(401).json({ message: "No autorizado" });
@@ -11,7 +11,11 @@ export const requireAuth = ({ tokenService }) => {
             req.user = user
             next();            
         } catch (error) {
-            return res.status(503).json({ message: "Token expirado o inválido" })
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({ message: "Token expirado", code: "TOKEN_EXPIRED" })
+            }
+
+            return res.status(403).json({ message: "Token inválido" })
         }
     }
 };
